fe2dc4c019013152366bc2fcc1f1be2a1926c2a2
Vasantha135 <ragimekalapallivasantha@gmail.com>
2026-01-03 14:33:41 +0530

    BannerAPP

--- BannerAPP/App/src/Main.java
+++ BannerAPP/App/src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Map;
 
 /**
  * OOPS Banner Application
@@ -12,12 +12,15 @@
 public class Main
 {
 
+
+
     public static void Uc1()
     {
         System.out.println("\"OOPS\" Object Oriented Programming System\n");
     }
 
-    public static void Uc2() {
+    public static void Uc2()
+    {
 
         String[] O = {
                 "  ******  ",
@@ -31,17 +34,18 @@ public class Main
                 "  ******  "
         };
 
-        String[] P = {
-                "*********  ",
-                "**     ** ",
-                "**     ** ",
-                "**     **",
-                "********  ",
-                "**         ",
-                "**         ",
-                "**         ",
-                "**         "
-        };
+        String[] P =
+                {
+                        "*********  ",
+                        "**     ** ",
+                        "**     ** ",
+                        "**     **",
+                        "********  ",
+                        "**         ",
+                        "**         ",
+                        "**         ",
+                        "**         "
+                };
 
         String[] S =
                 {
@@ -57,7 +61,8 @@ public class Main
 
                 };
 
-        for (int i = 0; i < O.length; i++) {
+        for (int i = 0; i < O.length; i++)
+        {
             System.out.println(
                     O[i] + "   " +
                             O[i] + "   " +
@@ -144,10 +149,89 @@ public class Main
                 };
     }
 
+    private static void uc4() {
+
+        BannerCharacter[] characters = {
+                new BannerCharacter('O', getOPattern()),
+                new BannerCharacter('O', getOPattern()),
+                new BannerCharacter('P', getPPattern()),
+                new BannerCharacter('S', getSPattern())
+        };
+        printBannerUC4(characters);
+    }
+
+    // Inner Class for Encapsulation
+    static class BannerCharacter {
+        private char character;
+        private String[] pattern;
+
+        public BannerCharacter(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    private static void printBannerUC4(BannerCharacter[] characters) {
+        for (int row = 0; row < 9; row++) {
+            StringBuilder line = new StringBuilder();
+            for (BannerCharacter ch : characters) {
+                line.append(ch.getPattern()[row]).append(" ");
+            }
+            System.out.println(line.toString());
+        }
+    }
+
+    private static void uc5()
+    {
+        Map<Character, String[]> patternMap = buildPatternMap();
+        String word = "OOPS";
+        printBannerUC5(word, patternMap);
+    }
+
+    private static Map<Character, String[]> buildPatternMap()
+    {
+        Map<Character, String[]> map = new java.util.HashMap<>();
+        map.put('O', getOPattern());
+        map.put('P', getPPattern());
+        map.put('S', getSPattern());
+        return map;
+    }
+
+    private static void printBannerUC5(String word, Map<Character, String[]> map)
+    {
+        for (int row = 0; row < 9; row++) {
+            StringBuilder line = new StringBuilder();
+            for (char ch : word.toCharArray()) {
+                line.append(map.get(ch)[row]).append(" ");
+            }
+            System.out.println(line.toString());
+        }
+    }
+
     public static void main(String[] args)
     {
+        System.out.println("USE-CASE-1");
+        System.out.println("----------------------------------------------------------------------------------------");
         Uc1();
+        System.out.println("USE-CASE-2");
+        System.out.println("----------------------------------------------------------------------------------------");
         Uc2();
+        System.out.println("USE-CASE-3");
+        System.out.println("----------------------------------------------------------------------------------------");
         Uc3();
+        System.out.println("USE-CASE-4");
+        System.out.println("----------------------------------------------------------------------------------------");
+        uc4();
+        System.out.println("USE-CASE-5");
+        System.out.println("----------------------------------------------------------------------------------------");
+        uc5();
     }
 }
