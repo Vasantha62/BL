58bf3191533328328313017fc530f5fa221e1db0
Vasantha135 <ragimekalapallivasantha@gmail.com>
2026-01-06 11:03:19 +0530

    FEAT : added UC-2 Display oops bannerApp using Arrays and loop

--- oopsBanner_app/src/UC2.java
+++ oopsBanner_app/src/UC2.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
 
+/**
+ * UC2 – Hardcoded OOPS Banner
+ *
+ * This use case prints the OOPS banner
+ * using hardcoded string arrays and loops.
+ *
+ * @author Vasantha
+ * @version 1.0
+ */
 public class UC2 {
+
+    public static void main(String[] args) {
+
+        // Pattern for letter O
+        String[] O = {
+                "  ******  ",
+                " **    ** ",
+                "**      **",
+                "**      **",
+                "**      **",
+                "**      **",
+                "**      **",
+                " **    ** ",
+                "  ******  "
+        };
+
+        // Pattern for letter P
+        String[] P = {
+                "*********  ",
+                "**     ** ",
+                "**     ** ",
+                "**     **",
+                "********  ",
+                "**         ",
+                "**         ",
+                "**         ",
+                "**         "
+        };
+
+        // Pattern for letter S
+        String[] S = {
+                "   *****  ",
+                " **       ",
+                "**        ",
+                "  **      ",
+                "    ***   ",
+                "       ** ",
+                "        **",
+                "       **",
+                "  *****   "
+        };
+
+        // Loop to print banner row by row
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "   " + O[i] + "   " + P[i] + "   " + S[i]);
+        }
+    }
 }
