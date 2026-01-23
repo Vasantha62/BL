8904a7feaee753914e123bb7a186139837a1ce17
Vasantha135 <ragimekalapallivasantha@gmail.com>
2026-01-23 07:05:41 +0530

    FEAT:added Arrays(program to find minimum value in an array)

--- Arrays/FindMinimumValueOfArray.java
+++ Arrays/FindMinimumValueOfArray.java
@@ -1,7 +1,7 @@
 public class FindMinimumValueOfArray {
     public static void main(String[] args) {
         int[] a = {88, 2, 33, 57};
-        int min = a[0];
+        int min = a[0]; // assume
 
         for (int i = 1; i < a.length; i++) {
             if (a[i] < min) {
