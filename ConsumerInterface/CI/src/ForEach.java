3d67155b84cc5e1e8f29cf38319f20bbc3c87902
Vasantha135 <ragimekalapallivasantha@gmail.com>
2026-01-23 17:59:20 +0530

    FEAT:added ConsumerInterface(use Consumer with forEach on List of strings)

--- ConsumerInterface/CI/src/ForEach.java
+++ ConsumerInterface/CI/src/ForEach.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
+
 public class ForEach {
     public  static  void main(String[] args){
         List<String> names = Arrays.asList("Vasantha", "Manju", "manu");
