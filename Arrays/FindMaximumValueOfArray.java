9bb7c29cc553c536c3128385bc06dceed94796d8
Vasantha135 <ragimekalapallivasantha@gmail.com>
2026-01-23 07:10:10 +0530

    FEAT:added Arrays(program to find maximum value in an array)

--- Arrays/FindMaximumValueOfArray.java
+++ Arrays/FindMaximumValueOfArray.java
@@ -3,6 +3,7 @@ public class FindMaximumValueOfArray {
         int[] a = {88, 2, 33, 57};
         int max= a[0]; // assume
 
+
         for (int i = 1; i < a.length; i++) {
             if (a[i] > max) {
                 max = a[i]; 
