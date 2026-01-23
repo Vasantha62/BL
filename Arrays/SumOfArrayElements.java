b635abafaea66a54dac5da53130c92fc39e85906
Vasantha135 <ragimekalapallivasantha@gmail.com>
2026-01-23 07:10:52 +0530

    FEAT:added Arrays(program to calculate sum of array elements)

--- Arrays/SumOfArrayElements.java
+++ Arrays/SumOfArrayElements.java
@@ -1,6 +1,7 @@
 public class SumOfArrayElements {
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5};
+
         int sum = 0;
 
         for (int i = 0; i < arr.length; i++) {
