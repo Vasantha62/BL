cb03f34c845d25ef51f829dba7c08706069e5518
Vasantha135 <ragimekalapallivasantha@gmail.com>
2026-01-23 17:32:07 +0530

    FEAT:added DAY-23(convert Integer list to Double using lambda expression)

--- LambdaExpression(DAY23)/LME/UC1.4/src/Main.java
+++ LambdaExpression(DAY23)/LME/UC1.4/src/Main.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
-
 public class Main {
     public static  void main(String[] args) {
-
 
         List<Integer> numbers = new ArrayList<>();
         numbers.add(5);
