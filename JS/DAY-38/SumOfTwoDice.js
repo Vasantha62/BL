396116689a39ffc2ce0011f42828758942c9724c
Vasantha135 <ragimekalapallivasantha@gmail.com>
2026-01-28 10:40:34 +0530

    FEAT:added DAY-38(Generate two dice numbers and log their sum)

---
+++ JS/DAY-38/SumOfTwoDice.js
@@ -0,0 +1,8 @@
+// Generate two dice numbers
+let dice1 = Math.floor(Math.random()*6)+1;
+let dice2 = Math.floor(Math.random()*6)+1;
+// Add dice values
+let sum = dice1+dice2;
+console.log("Dice1:", dice1);
+console.log("Dice2:", dice2);
+console.log('sum of Two dices is:' , sum);
