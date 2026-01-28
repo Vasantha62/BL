eeb19ea119d0eceec70225efbd8884066eaf2a20
Vasantha135 <ragimekalapallivasantha@gmail.com>
2026-01-28 10:45:44 +0530

    FAET:added DAY-38(Convert length and width from feet to meters)

---
+++ JS/DAY-38/RectangularPlotInMeeters.js
@@ -0,0 +1,10 @@
+const FEET_TO_METER = 0.3048;
+
+let lengthFt = 60;
+let widthFt = 40;
+
+let lengthMeter = lengthFt * FEET_TO_METER;
+let widthMeter = widthFt * FEET_TO_METER;
+
+console.log("Length in meters:", lengthMeter);
+console.log("Width in meters:", widthMeter);
