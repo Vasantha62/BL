3349e2951b5ac8c75a3f763a07a662ddf80b7e43
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-02-02 11:13:28 +0530

    feat

---
+++ JS/DAY-40/AddressBook/UC2_ValidateContact.js
@@ -0,0 +1,40 @@
+
+/**
+ * UC2: Test Contact Validation
+ */
+
+const Contact = require("./Contact");
+
+try {
+    let contact = new Contact(
+        "Vasantha",
+        "Reddy",
+        "MG Road",
+        "Chennai",
+        "Tamilnadu",
+        "600001",
+        "9876543210",
+        "abc.xyz@bridgelabz.co.in"
+    );
+
+    console.log("Contact validated successfully");
+} catch (error) {
+    console.error("Validation Error:", error.message);
+}
+
+// Invalid Contact Test
+try {
+    new Contact(
+        "vas",
+        "r",
+        "St",
+        "Che",
+        "TN",
+        "060001",
+        "12345",
+        "abc@gmail"
+    );
+} catch (error) {
+    console.error("Validation Error:", error.message);
+}
+
