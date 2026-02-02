7a1677253960e3071972024f5b6a0f58009bcbe6
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-02-02 11:12:55 +0530

    feat:Added DAY-40(UC1: Create Contact and display using toString method)

---
+++ JS/DAY-40/AddressBook/UC1_CreateContact.js
@@ -0,0 +1,21 @@
+
+
+const Contact = require("./Contact");
+
+// Create Contact
+let contact = new Contact(
+    "Vasantha",
+    "R",
+    "MG Road",
+    "Chennai",
+    "Tamilnadu",
+    "600001",
+    "9876543210",
+    "vasantha@gmail.com"
+);
+
+// Display Contact
+console.log(contact.toString());
+
+
+
