ee353dc6401507a63ee4e29d86acdfbd190c5494
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-02-02 11:09:14 +0530

    feat:Added DAY-40(Implement and display contact sorting by city, state, and zip-UC12)

---
+++ JS/DAY-40/AddressBook/UC12_SortByCityStateZip.js
@@ -0,0 +1,28 @@
+
+const Contact = require("./Contact");
+const AddressBook = require("./AddressBook");
+
+try {
+    let book = new AddressBook();
+
+    // Add Contacts
+    book.addContact(new Contact("Rohit", "Sharma", "Park Street", "Chennai", "Tamilnadu", "600002", "9876543212", "rohit@bridgelabz.co.in"));
+    book.addContact(new Contact("Vasantha", "Reddy", "MG Road", "Chennai", "Tamilnadu", "600001", "9876543210", "abc.xyz@bridgelabz.co.in"));
+    book.addContact(new Contact("Anusha", "Kumar", "Brigade Road", "Bangalore", "Karnataka", "560001", "9876543211", "anusha@bridgelabz.co.in"));
+    book.addContact(new Contact("Priya", "Singh", "MG Road", "Bangalore", "Karnataka", "560002", "9876543213", "priya@bridgelabz.co.in"));
+
+    console.log("Contacts sorted by City:");
+    let citySorted = book.sortByCity();
+    book.showContacts(citySorted);
+
+    console.log("\nContacts sorted by State:");
+    let stateSorted = book.sortByState();
+    book.showContacts(stateSorted);
+
+    console.log("\nContacts sorted by Zip:");
+    let zipSorted = book.sortByZip();
+    book.showContacts(zipSorted);
+
+} catch (error) {
+    console.error("Error:", error.message);
+}
